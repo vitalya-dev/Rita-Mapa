8147c0186f3044f26d77d6673978b2e83e7b49c9
Vitalya <vitalya.dev@gmail.com>
2026-03-22 14:00:18 +0300

    feat: добавлен рефакторинг в функцию create_map и отображение всех полуостровов

--- 1.py
+++ 1.py
@@ -1,52 +1,41 @@
 import folium
 
-# 1. Создаем базовую карту мира. 
-# location=[20, 0] - центр карты, zoom_start=2 - начальный масштаб
-my_map = folium.Map(location=[20, 0], zoom_start=2, tiles="CartoDB positron")
+def create_map():
+    # Создаем базовую карту мира
+    my_map = folium.Map(location=[20, 0], zoom_start=2, tiles="CartoDB positron")
 
-# --- ДОБАВЛЯЕМ ОБЪЕКТЫ ---
+    # --- ДОБАВЛЯЕМ ПОЛУОСТРОВА ---
+    # Словарь с названиями полуостровов и их примерными координатами [широта, долгота]
+    peninsulas = {
+        "Крымский": [45.3, 34.4],
+        "Гибралтар": [36.14, -5.35],
+        "Пиренейский": [40.0, -4.0],
+        "Аппенинский": [42.0, 14.0],
+        "Скандинавский": [63.0, 14.0],
+        "Аравийский": [23.0, 46.0],
+        "Индокитай": [15.0, 102.0],
+        "Индостан": [20.0, 77.0],
+        "Малая Азия": [39.0, 32.0],
+        "Сомали": [8.0, 48.0],
+        "Аляска": [64.0, -150.0],
+        "Лабрадор": [54.0, -64.0],
+        "Калифорния": [28.0, -113.5],
+        "Флорида": [28.0, -81.5],
+        "Юкатан": [19.5, -89.0]
+    }
 
-# 1. Полуостров (например, Флорида). Поставим обычный маркер
-folium.Marker(
-    location=[28.0, -81.5], # Координаты Флориды
-    popup="<b>Полуостров Флорида</b>",
-    tooltip="Нажми меня",
-    icon=folium.Icon(color="blue", icon="info-sign")
-).add_to(my_map)
+    # Проходим циклом по всем полуостровам и добавляем их на карту
+    for name, coords in peninsulas.items():
+        folium.Marker(
+            location=coords,
+            popup=f"<b>Полуостров {name}</b>",
+            tooltip=name,
+            icon=folium.Icon(color="blue", icon="info-sign")
+        ).add_to(my_map)
 
-# 2. Остров (например, Мадагаскар). 
-folium.Marker(
-    location=[-18.7669, 46.8691],
-    popup="<b>Остров Мадагаскар</b>",
-    tooltip="Мадагаскар",
-    icon=folium.Icon(color="green", icon="leaf")
-).add_to(my_map)
+    # Сохраняем результат
+    my_map.save("geography_map.html")
+    print("Карта с полуостровами готова! Открой файл geography_map.html в браузере.")
 
-# 3. Пустыня (например, Сахара). Нарисуем желтый круг, покрывающий территорию
-folium.Circle(
-    location=[23.8, 11.2], # Центр Сахары
-    radius=1500000,        # Радиус в метрах (1500 км)
-    popup="Пустыня Сахара",
-    color="orange",        # Цвет границы
-    fill=True,
-    fill_color="yellow"    # Цвет заливки
-).add_to(my_map)
-
-# 4. Горная система (например, Уральские горы). Нарисуем линию вдоль хребта
-ural_mountains_coords = [
-    [68.0, 60.0], # Северная часть
-    [60.0, 59.0], # Средняя часть
-    [52.0, 58.0]  # Южная часть
-]
-folium.PolyLine(
-    locations=ural_mountains_coords,
-    color="brown",
-    weight=5, # Толщина линии
-    tooltip="Уральские горы"
-).add_to(my_map)
-
-
-# --- СОХРАНЯЕМ РЕЗУЛЬТАТ ---
-# Сохраняем нашу карту в HTML файл
-my_map.save("geography_map.html")
-print("Карта готова! Открой файл geography_map.html в браузере.")
+# Запускаем нашу функцию
+create_map()
